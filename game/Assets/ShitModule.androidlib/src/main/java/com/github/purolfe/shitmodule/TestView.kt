00d98cc1842918b808df4c3adf59e0547f1d19fe
purofle <purofle@gmail.com>
2025-10-05 00:23:17 +0800

    添加 compose 依赖

--- game/Assets/ShitModule.androidlib/src/main/java/com/github/purolfe/shitmodule/TestView.kt
+++ game/Assets/ShitModule.androidlib/src/main/java/com/github/purolfe/shitmodule/TestView.kt
@@ -1,9 +1,15 @@
 package com.github.purolfe.shitmodule
 
+import androidx.compose.runtime.Composable
 import org.mozilla.geckoview.GeckoSession
 
 class TestView {
     init {
         GeckoSession.PRIORITY_HIGH
+    }
+
+    @Composable
+    fun TestUI() {
+
     }
 }
