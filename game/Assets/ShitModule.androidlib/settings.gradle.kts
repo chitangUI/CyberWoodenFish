db67a59c1bf144aaaaa192e24450aad81658f1de
purofle <purofle@gmail.com>
2025-10-05 00:07:16 +0800

    我草你妈 Unity
    - 升级 Unity 版本到 6000.2.6f2
    - 升级 Gradle & AGP 版本
    - 新增 `ShitModule.androidlib` (TODO)

---
+++ game/Assets/ShitModule.androidlib/settings.gradle.kts
@@ -0,0 +1,27 @@
+import org.gradle.kotlin.dsl.maven
+import java.net.URI
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        maven("https://maven.mozilla.org/maven2/")
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Shit Module"
+include(":app")
---
+++ game/Assets/ShitModule.androidlib/src/main/java/com/github/purolfe/shitmodule/TestView.kt
@@ -0,0 +1,9 @@
+package com.github.purolfe.shitmodule
+
+import org.mozilla.geckoview.GeckoSession
+
+class TestView {
+    init {
+        GeckoSession.PRIORITY_HIGH
+    }
+}
